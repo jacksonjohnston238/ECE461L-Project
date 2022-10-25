f1b9f9f62cf85823846fe665773afbb8e998a27d
nathanbstern <70401972+nathanbstern@users.noreply.github.com>
2022-10-25 09:30:34 -0500

    Add the Login Component (#45)
    * initial commit for Login
    
    * remove extra frontend folder

---
+++ app/src/components/Login.js
@@ -0,0 +1,47 @@
+import { Stack, Box, TextField, Button } from "@mui/material"
+import { useState } from "react"
+
+function Login(){
+
+    const [username, setUsername] = useState('')
+    const [userid, setUserID] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+        <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+            <Box sx={{border: 4, borderColor: 'blueviolet', p:5}}>
+            <Stack spacing={2}>
+                <h1>Login</h1>
+                <h2>Username</h2>
+                <TextField value={username === 0 ? '' : username} type="string" id="outlined-basic" label="Enter Username" variant="outlined" onChange={(e) =>{
+                    setUsername(e.target.value)
+                }}/>
+                <h2>User ID</h2>
+                <TextField value={userid === 0 ? '' : userid} type="string" id="outlined-basic" label="Enter User ID" variant="outlined" onChange={(e) =>{
+                    setUserID(e.target.value)
+                }}/>
+                <h2>Password</h2>
+                <TextField value={password === 0 ? '' : password} type="string" id="outlined-basic" label="Enter Password" variant="outlined" onChange={(e) =>{
+                    setPassword(e.target.value)
+                }}/>
+                
+                <Button variant='contained' size= 'large'
+                    onClick={() => 
+                    /*ToDo: fill in rerouting and verification */alert("click")}>
+                    Login
+                </Button>
+                </Stack>
+                <p>Don't have an account? <a href='link'>Sign Up</a></p>
+            </Box>
+        </div>
+    )
+}
+
+export default Login
