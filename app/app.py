08e9ac8a926de3cb89bfe04c7332569e1491b30d
Shreekrishna Rout <shreekrishna.rout@utexas.edu>
2022-10-28 18:01:13 -0500

    Added Database Shared to Group

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+# from and import statements in order to access specific functions with these libraries
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://ecedatabaseuser:wHHLGhPoC95vByvV@cluster0.gopiidc.mongodb.net/?retryWrites=true&w=majority")
+# Initializes the database to be the test database in the client in MongoDB
+database = client.ECE461L-FinalProject-Database
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 
@@ -39,3 +44,7 @@ def leaveProject(projectid):
     return {
         'response' : f'Left {projectid}'
     }
+
+
+# Close the database collection
+client.close()
